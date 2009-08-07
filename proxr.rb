fb3620d02027ce9a5da8b6beae6175350d0df033
Matt Scilipoti <matt@scilipoti.name>
2009-08-07 11:42:29 -0400

    Rename to proxr.rb

--- proxr.rb
+++ proxr.rb
@@ -1,31 +1,30 @@
 require 'rubygems'
 require 'serialport'
+require 'timeout'
 
-class ProXR
-  attr_accessor :serial_port
-
+class ProXR < SerialPort
   SUCCESS = 85
-  def initialize(device = "/dev/ttyS0", baud_rate = 115200)
-    @serial_port = SerialPort.new device, baud_rate
+
+  def initialize(device = "/dev/ttyS0", *params)
+    default_params = {:baud_rate => 115200}
+    super device, default_params.merge(params)
   end
 
   def send_command(*cmds)
-    serial_port.write 254.chr
-    cmds.each {|cmd| serial_port.write cmd.chr }
+    Timeout::timeout(0.5) do
+      write 254.chr
+      cmds.each {|cmd| write cmd.chr }
 
-    serial_port.getc
+      getc
+    end
   end
 
   def reporting_mode
     send_command(27)
   end
 
-  def getc
-    serial_port.getc
-  end
-
-  def read_voltage(relay_number, bank_number)
-    send_command(150, bank_number)
+  def read_voltage(channel)
+    send_command(150 + channel)
   end
 
   def relay_on(relay_number, bank_number)
@@ -38,8 +37,13 @@ class ProXR
     send_command relay_off_cmd, bank_number
   end
 
-
-
+  def show_all_voltages
+    p = ProXR.new
+    puts "Ch:Volt"
+    (0..7).each do |channel|
+      puts " #{channel}:#{p.read_voltage(channel)}"
+    end
+  end
 
 end
 
@@ -47,14 +51,15 @@ if $0 == __FILE__
   require 'test/unit'
   class TestReportingMode < Test::Unit::TestCase
     def setup
-      @serial_port = ProXR.new
+      @serial_port = ProXR.new "/dev/ttyS0"
     end
+
     def test_should_indicate_it_is_in_reporting_mode
       assert_equal ProXR::SUCCESS, @serial_port.reporting_mode
     end
 
-    def test_voltage_at_0_1_should_be_zero
-      assert_equal 0, @serial_port.read_voltage(0, 0)
+    def test_voltage_at_0_1_should_be_255
+      assert_equal 255, @serial_port.read_voltage(0)
     end
 
     def test_relay_on_0_1
